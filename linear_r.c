5166b262805f6c9a71c93a941b3a9ff76303aba4
chethana2121 <93599484+chethana2121@users.noreply.github.com>
2021-11-09 12:49:40 +0530

    linear_search recurrsion program

---
+++ linear_r.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int linearsearch(int [],int,int,int);
+int main(){ 
+    int n,ele,pos,a[10],i;
+    printf("enter the no of elements:");
+    scanf("%d",&n);
+    printf("enter the elements:");
+    for(i=0;i<n;i++){
+            scanf("%d",&a[i]);
+            }
+    printf("enter the element to be found:");
+    scanf("%d",&ele);
+    pos = linearsearch(a,n,ele,0);
+    if(pos == -1){
+        printf("element found at %d position",pos+1);
+    }
+    }
+int linearsearch(int a[10],int n,int ele,int pos){
+        if(pos == -1)
+                return 0;
+        else if(a[pos]==ele){
+            return pos;
+        }
+        else{
+        return linearsearch(a,n,ele,pos+1);
+        }
+return -1;
+
+}
